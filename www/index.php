bbca853730ec2d8f3a8413e96be0fead2dc31262
jeremy jumpertz <jeremy.jumpertz@gmail.com>
2021-07-21 20:23:43 +0200

    ajout d'un peu de style

--- www/index.php
+++ www/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="public/css/framework.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 
@@ -12,3 +22,6 @@ echo "<p>server is running:" .$redis->ping() . "</p><br>";
 $redis->set("user", "Sam le bg");
 
 echo "<p>".$redis->get('user') ."</p>";
+?>
+</body>
+</html>
